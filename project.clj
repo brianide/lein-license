f6e32ab0f3871b3609dfca2c0c20e7e1b44a635e
brianide <brianide@gmail.com>
2019-03-10 17:04:27 -0500

    Fixed build issue and removed dead code

--- project.clj
+++ project.clj
@@ -1,14 +1,11 @@
-(defproject org.clojars.brianide/lein-license "0.1.9-SNAPSHOT"
+(defproject org.clojars.brianide/lein-license "0.1.10-SNAPSHOT"
   :description "Project-Level License Management."
   :url "https://github.com/brianide/lein-license"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"
             :key "mit"
             :year 2015}
-  :dependencies [^:source-dep [org.yaml/snakeyaml "1.17"]
-                 ^:source-dep [rewrite-clj "0.6.1" :exclusions [org.clojure/clojure]]
+  :dependencies [^:source-dep [rewrite-clj "0.6.1" :exclusions [org.clojure/clojure]]
                  ^:source-dep [cheshire "5.8.0" :exclusions [org.clojure/clojure]]]
-  :plugins [[lein-isolate "0.1.1"]]
-  :middleware [leiningen.isolate/middleware]
-  :eval-in :leiningen
+  :eval-in-leiningen true
   :pedantic? :abort)
--- src/leiningen/license/fetch.clj
+++ src/leiningen/license/fetch.clj
@@ -3,71 +3,12 @@
             [leiningen.core.main :as main]
             [clojure.string :as string]
             [clojure.set :refer [rename-keys]]
-            [cheshire.core :as json])
-  (:import [org.yaml.snakeyaml Yaml]
-           [org.yaml.snakeyaml.scanner ScannerException]))
+            [cheshire.core :as json]))
 
 ;; ## URLs
 
 (def ^:dynamic *raw-prefix*
   "https://api.github.com/licenses/")
-
-;; ## Parser
-
-(def ^:private license-regex
-  #"(?s)^---\n(.+)\n---\n(.*)$")
-
-(def ^:private required-keys
-  #{:description :title :permalink})
-
-(defn- ensure-required-keys
-  [data]
-  (if-let [missing (seq (remove (set (keys data)) required-keys))]
-    (throw
-      (Exception.
-        (str "required keys missing: " (pr-str (vec missing)))))
-    data))
-
-(def ^:private parse-license-yaml
-  "Parse a YAML string into a map. Returns a map with either the single key
-   `:value` or `:error`."
-  (let [yaml (Yaml.)]
-    (fn [^String data license-key]
-      (try
-        (if-let [value (.load yaml data)]
-          (if (or (map? value) (instance? java.util.LinkedHashMap value))
-            (->> (for [[k v] value
-                       :let [s (string/replace (name k) "_" "-")
-                             v' (if (instance? java.util.ArrayList v)
-                                  (mapv keyword v)
-                                  v)]]
-                   [(keyword s) v'])
-                 (into
-                   {:permalink (format "/_licenses/%s.txt" license-key)})
-                 (ensure-required-keys)
-                 (hash-map :value))
-            {:error "YAML value does not represent a map."})
-          {:error "could not read YAML value."})
-        (catch ScannerException ex
-          {:error (str "invalid YAML: " (.getMessage ex))})
-        (catch Exception ex
-          {:error (.getMessage ex)})))))
-
-(defn- normalize-template
-  [^String s]
-  (string/replace s #"(^[\n\r]+|[\n\r]+$)" ""))
-
-(defn- parse-license
-  "Parse raw license data, returning a map of either `:error` or the
-   desired data."
-  [^String data license-key]
-  (when-let [[_ info template] (re-find license-regex data)]
-    (let [{:keys [value error]} (parse-license-yaml info license-key)]
-      (if error
-        {:error error}
-        (assoc value
-               :text (normalize-template template)
-               :key  license-key)))))
 
 ;; ## License I/O
 
